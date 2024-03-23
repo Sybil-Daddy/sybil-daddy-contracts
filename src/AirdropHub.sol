4f09dbf30e61bf5ebdf9faab481ecfb2bea7409b
lordmike007 <manujasan23@gmail.com>
2024-03-24 01:25:23 +0530

    feat: core logic for Airdrop contract

--- src/AirdropHub.sol
+++ src/AirdropHub.sol
@@ -1,25 +1,65 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
-contract AirdropFactory {} // generate a new Airdrop contract for each airdrop
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AirdropFactory {
+    function createNewAirdrop() public {
+        // only Admin
+    } // call Airdropmain()
+} // generate a new Airdrop contract for each airdrop
 
 contract AirdropMain {
     mapping(address => uint256) addressAmountEligibility; //address => airdrop ammount
     mapping(uint256 => uint256) fidAmountEligibility; // fid => airdrop ammount
-    mapping(address => uint256) linkedFid; // address => linked fid
+    mapping(uint256 => address) linkedFid; // address => linked fid, for dynamic wallets ( AA wallets )
+    ERC20 airdropToken;
 
-    constructor() {} // pull that amount of ERC20 tokens from wallet
+    constructor(
+        ERC20 airdropToken_,
+        address[] memory eligibleAddresses,
+        uint256[] memory addressAmounts,
+        uint256[] memory eligibleFIDs,
+        uint256[] memory fidAmounts
+    ) {
+        require(eligibleAddresses.length == addressAmounts.length);
+        require(eligibleFIDs.length == fidAmounts.length);
+        airdropToken = airdropToken_;
+
+        uint256 totalAmount = 0;
+
+        for (uint256 i = 0; i < eligibleAddresses.length; i++) {
+            addressAmountEligibility[eligibleAddresses[i]] = addressAmounts[i];
+            totalAmount += addressAmounts[i];
+        }
+
+        for (uint256 i = 0; i < eligibleFIDs.length; i++) {
+            fidAmountEligibility[eligibleFIDs[i]] = fidAmounts[i];
+            totalAmount += fidAmounts[i];
+        }
+
+        airdropToken.transferFrom(msg.sender, address(this), totalAmount);
+    }
 
     function claimAirdrop() public {
         require(addressAmountEligibility[msg.sender] > 0);
         uint256 amount = addressAmountEligibility[msg.sender];
         addressAmountEligibility[msg.sender] = 0;
 
-        //send the token as airdrop
-    } // give it to him the same amount
+        airdropToken.transfer(msg.sender, amount);
+    }
 
     function linkAddressToFid(uint256 fid, address addressToLink) public {
         //only admin role
-        linkedFid[addressToLink] = fid;
+        linkedFid[fid] = addressToLink;
+    }
+
+    function claimAirdropForFID(uint256 fid) public {
+        require(linkedFid[fid] == msg.sender);
+
+        uint256 amount = fidAmountEligibility[fid];
+        fidAmountEligibility[fid] = 0;
+
+        airdropToken.transfer(msg.sender, amount);
     }
 }
